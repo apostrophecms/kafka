e76b87765ac021efbe2a98ec7680d8e12cf01999
cravier <clement.ravier_ext@michelin.com>
2023-10-15 23:13:37 +0200

    First @apostrophecms/kafka module version

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
-const fs = require('fs');
-const path = require('path');
+const methods = require('./lib/methods');
+const handlers = require('./lib/handlers');
 
 module.exports = {
-  bundle: {
-    directory: 'modules',
-    modules: getBundleModuleNames()
+  options: {
+    label: 'Kafka',
+    ignoreNoCodeWarning: true,
+    debug: process.env.NODE_ENV !== 'production'
+  },
+
+  methods,
+  handlers,
+
+  init(self) {
+    /**
+     * A Kafka consumer object wrapper object config.
+     * The topics Map contain the information of wich topic we need to listen to and
+     * also which callback function(s) we need to call once a message is received.
+     * @typedef {
+     *   {
+     *     instance: Kafka.consumer,
+     *     topics: Map<String, Array<callback>>,
+     *   }
+     * } KafkaConsumer
+     */
+
+    /**
+     * Map of Object with kafka instances, consumer and producer.
+     * One entry per cluster connection configuration.
+     * @type {
+     *   Map<String, { instance: Kafka, consumer: KafkaConsumer, producer: Kafka.producer }>
+     * }
+     */
+    self.pool = new Map();
   }
 };
-
-function getBundleModuleNames() {
-  const source = path.join(__dirname, './modules/@apostrophecms');
-  return fs
-    .readdirSync(source, { withFileTypes: true })
-    .filter(dirent => dirent.isDirectory())
-    .map(dirent => `@apostrophecms/${dirent.name}`);
-}
